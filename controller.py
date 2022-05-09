1999627e174f2ab29991922174af740d1b0d68b4
trevorolson1 <trevor.olson.one@gmail.com>
2022-05-08 21:14:00 -0500

    Yessir
    Yes

--- controller.py
+++ controller.py
@@ -15,15 +15,26 @@ class Controller(QMainWindow, Ui_MainWindow):
         self.resetButton.clicked.connect(lambda: self.reset())
 
     def indeed(self):
-        search_term = self.searchBox.toPlainText()
-        location_term = self.searchBox_2.toPlainText()
-        url = 'https://www.indeed.com/jobs?as_and=' + search_term + '&as_phr&as_any&as_not&as_ttl&as_cmp&jt=all&st&salary&radius=25&l=' + location_term + '%2C%20NE&fromage=any&limit=50&sort&psf=advsrch&from=advancedsearch&vjk=ae9008d2f6729286'
-        page = requests.get(url)
-        soup = BeautifulSoup(page.text, 'html.parser')
+        i= 0
+        for i in range(0,10):
+            search_term = self.searchBox.toPlainText()
+            location_term = self.searchBox_2.toPlainText()
+            url = 'https://www.indeed.com/jobs?as_and=' + search_term + '&as_phr&as_any&as_not&as_ttl&as_cmp&jt=all&st&salary&radius=25&l=' + location_term + '%2C%20NE&fromage=any&limit=50&sort&psf=advsrch&from=advancedsearch&vjk=ae9008d2f6729286'
+            page = requests.get(url)
+            soup = BeautifulSoup(page.text, 'html.parser')
+            job_title = soup.find('h2', class_='jobTitle').text
+            #employer_name = soup.find('span', class_='companyName').text
+            #location = soup.find('div', class_='companyLocation').text
+            self.listWidget.addItem('#{}. {}'.format(i,job_title))
+
+    #def linkedin(self):
+        #url = 'https://www.linkedin.com/jobs/search/?geoId=100739428&keywords=' + search_term + '&location=' + location_term
+    #def google(self):
+        #url = 'https://www.google.com/search?q=' + search_term + '&rlz=1C1CHBF_en&oq=google+job+s&aqs=chrome.1.69i57j0i433i512j0i512l5j0i10i512j0i512l2&sourceid=chrome&ie=UTF-8&ibp=htl;jobs&sa=X&sqi=2&ved=2ahUKEwjs1ZLLptH3AhWchP0HHcjsCNgQutcGKAF6BAggEAY&sxsrf=ALiCzsYk8LG6xYJPn7SALKDVStz4i5QVQg:1652060938618#fpstate=tldetail&htivrt=jobs&htichips=city:8WYPEnHkrIl-8kaKidp64Q%3D%3D&htischips=city;8WYPEnHkrIl-8kaKidp64Q%3D%3D:' + location_term +'&htidocid=LQQoNIk0r3cAAAAAAAAAAA%3D%3D'
+    #def ziprecruiter(self):
+        #url = 'https://www.ziprecruiter.com/jobs-search?search=' search_term + '&location=' + location_term
 
 
-        #company_name = job.find('span', class_='companyName').text.replace(' ', '')
-        self.listWidget.addItem(soup.find('div', class_='slider_item').text)
 
     def reset(self):
         x = '6'
