09d40120b2118f8d0f99bad641bba94cf0c72c6d
trevorolson1 <trevor.olson.one@gmail.com>
2022-05-08 19:40:17 -0500

    Bing bong
    Hello

--- controller.py
+++ controller.py
@@ -1,8 +1,10 @@
 from bs4 import BeautifulSoup
 from view import Ui_MainWindow
-import lxml
+
 import requests
 from PyQt5.QtWidgets import *
+import time
+
 
 
 class Controller(QMainWindow, Ui_MainWindow):
@@ -13,16 +15,16 @@ class Controller(QMainWindow, Ui_MainWindow):
         self.resetButton.clicked.connect(lambda: self.reset())
 
     def indeed(self):
-        search_term = 'software'  # self.searchBox.toPlainText()
-        location_term = 'omaha'  # self.searchBox_2.toPlainText()
+        search_term = self.searchBox.toPlainText()
+        location_term = self.searchBox_2.toPlainText()
         url = 'https://www.indeed.com/jobs?as_and=' + search_term + '&as_phr&as_any&as_not&as_ttl&as_cmp&jt=all&st&salary&radius=25&l=' + location_term + '%2C%20NE&fromage=any&limit=50&sort&psf=advsrch&from=advancedsearch&vjk=ae9008d2f6729286'
         page = requests.get(url)
         soup = BeautifulSoup(page.text, 'html.parser')
-        job = soup.find('div', class_='slider_item')
+
 
         #company_name = job.find('span', class_='companyName').text.replace(' ', '')
-
-        self.listWidget.addItem(job)
+        self.listWidget.addItem(soup.find('div', class_='slider_item').text)
 
     def reset(self):
-        self.listWidget.addItem("hello world")
+        x = '6'
+        self.listWidget.addItem(x)
