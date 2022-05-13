c6d00c154b967beaddaae5a18310110d04fd2042
nhzl <100879807+nhzl@users.noreply.github.com>
2022-05-13 16:35:14 -0500

    Test

---
+++ test.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def reset_button(self):
+        self.assertEqual(True, False)  # add assertion here
+    def search_button(self):
+        self.assertEqual()
+
+
+if __name__ == '__main__':
+    unittest.main()
