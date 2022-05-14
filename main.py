b536c8b79dd5a43ad027a6dc5986850d299cca22
trevorolson1 <trevor.olson.one@gmail.com>
2022-05-13 21:43:16 -0500

    Hello
    Yes

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 from controller import *
-#import unittest
-
-"""class test_search_input(unittest.TestCase):
-    def test_string(self):
-        from self.search() import
-        x = self.searchBox.toPlainText()
-        self.assertEqual(x.isalpha(),True)"""
-
 
 def main():
     app = QApplication([])
